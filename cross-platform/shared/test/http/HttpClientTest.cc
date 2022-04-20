83efb971e53623373f3e8f0c9101938d27131325
ycagri <akkayacagri@gmail.com>
2022-04-20 12:08:05 +0300

    test: test assertions are updated.

--- cross-platform/shared/test/http/HttpClientTest.cc
+++ cross-platform/shared/test/http/HttpClientTest.cc
@@ -1,13 +1,18 @@
 #include <gtest/gtest.h>
 #include <http/HttpClient.hpp>
-#include "boost/assign.hpp"
 
 using namespace client;
 
-TEST(HttpClientTest, GetRequestTest){
+TEST(HttpClientTest, GetUserRequestTest){
     HttpClient* req = new HttpClient();
     std::string response = req->getRequest("api.github.com", "/users/ycagri", "443");
-    std::cout << response << std::endl;
-    EXPECT_NE(0, response.length());
+    EXPECT_TRUE(response.rfind("{\"login\":\"ycagri\"") == 0);
+    delete req;
+}
+
+TEST(HttpClientTest, GetReposRequestTest){
+    HttpClient* req = new HttpClient();
+    std::string response = req->getRequest("api.github.com", "/users/ycagri/repos", "443");
+    EXPECT_TRUE(response.rfind("[{\"id\":341657976,") == 0);
     delete req;
 }
